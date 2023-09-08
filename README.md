092eb235b53fb181a73fc353782bf21897a079d9
fima rubin <fimarubin@fimas-MacBook-Pro.local>
2023-09-08 15:19:05 +0300

    Readme + donate + analytics

---
+++ README.md
@@ -0,0 +1,17 @@
+# Dr. Strange in Ukraine
+
+## Reason for this project
+
+This project combines technology and help for Ukraine. Since 2014 there has been an ongoing war between ruzzia and Ukraine, and on the 24th of February 2022, ruzzia started a full invasion of Ukraine. With the help of soldiers who courageously fight for Ukraine, Ukraine's friends, such as USA, Great Britain and so on and so forth, who supply Ukraine with the weaponry, and caring people, who send money to the army, Ukraine has been successfuly fighting the occupants and showing them where their place is: in the swamps and dirt where they come from.
+
+But more and more people forget about the ongoing war. I have friends(from abroad) who ask me "Is the war still going on?" The media now broadcasts only the big events(the ones THEY consider big). I think that this is wrong. People should be aware of what is happening in Ukraine.
+
+And that is why I made this kind of game where you act as Doctor Strange from marvel universe to go from the Ukraine as it was before the war to Ukraine, that it is now, to Ukraine that has been defending its territories for over 9 years, to Ukraine the part of which was taken in so many ways, and in some ways forever. There is a donate button and a share button. I want you, the reader to help Ukraine, to share this page and to donate money to Ukraine. It is as much your fight as it is theirs. It is a fight between evil and good, between dark and light, between slavery and freedom, between old and new. It is a fight between russia and Ukraine. You can stand by and watch the fight, or you can take part in it, and help Ukraine win.
+
+## How to use this website
+
+Once you open up a page, it will show you a picture of a city in Ukraine as it was before the war, a slot with video and some general information. If you have seen doctor strange open portals, you know that he does it like this: ![Doctor Strange](./assets/actual_dr_strange.gif)
+
+To open a portal to Ukraine now, you just have to replicate his movements, like this: ![Tutorial](./assets/tutorial.gif). Your fingers' position will be shown with the purplish circles and the portal opens completely once it opens enough(you can see the progress in the bottom right corner). I hope you will do the right thing: support Ukraine and donate money. And if you are russian: either donate money, or fuck off.
+
+for any question write in [telegram](https://t.me/fubim)
--- js_source/positioned_functions.js
+++ js_source/positioned_functions.js
@@ -1,10 +1,69 @@
-var donateBtn = document.getElementById("donate");
+var donateBtns = Array.prototype.slice.call(document.getElementsByClassName("donate"));
 var shareBtn = document.getElementById("share");
 var nextBtn = document.getElementById("next");
 
-donateBtn.addEventListener('click', () => {
-  window.open("https://send.monobank.ua/jar/dzBdJ3737");
+donateBtns.forEach(element => {
+  element.addEventListener('click', () => {
+    window.open("https://uahelp.monobank.ua/");
+  });
 });
+
+/* This script supports IE9+ */
+(function() {
+  /* Opening modal window function */
+  function openModal() {
+      /* Get trigger element */
+      var modalTrigger = document.getElementsByClassName('jsModalTrigger');
+
+      /* Set onclick event handler for all trigger elements */
+      for(var i = 0; i < modalTrigger.length; i++) {
+          modalTrigger[i].onclick = function() {
+            var target = this.getAttribute('href').substr(1);
+            var modalWindow = document.getElementById(target);
+
+            modalWindow.classList ? modalWindow.classList.add('open') : modalWindow.className += ' ' + 'open'; 
+          }
+      }   
+  }
+
+  function closeModal(){
+    /* Get close button */
+    var closeButton = document.getElementsByClassName('jsModalClose');
+    var closeOverlay = document.getElementsByClassName('jsOverlay');
+
+    /* Set onclick event handler for close buttons */
+      for(var i = 0; i < closeButton.length; i++) {
+        closeButton[i].onclick = function() {
+          var modalWindow = this.parentNode.parentNode;
+
+          modalWindow.classList ? modalWindow.classList.remove('open') : modalWindow.className = modalWindow.className.replace(new RegExp('(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        }
+      }   
+
+    /* Set onclick event handler for modal overlay */
+      for(var i = 0; i < closeOverlay.length; i++) {
+        closeOverlay[i].onclick = function() {
+          var modalWindow = this.parentNode;
+
+          modalWindow.classList ? modalWindow.classList.remove('open') : modalWindow.className = modalWindow.className.replace(new RegExp('(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        }
+      }  
+
+  }
+
+  /* Handling domready event IE9+ */
+  function ready(fn) {
+    if (document.readyState != 'loading'){
+      fn();
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
+    }
+  }
+
+  /* Triggering modal window function after dom ready */
+  ready(openModal);
+  ready(closeModal);
+}());
 
 shareBtn.addEventListener('click', async () => {
   await navigator.clipboard.writeText('https://dr-strange-in-ukraine.onrender.com');
