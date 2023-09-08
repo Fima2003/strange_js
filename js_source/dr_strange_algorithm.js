4a97022248aee345f9c7897677a7334f186bb15a
fima rubin <fimarubin@fimas-MacBook-Pro.local>
2023-09-08 09:56:31 +0300

    Working website

--- js_source/dr_strange_algorithm.js
+++ js_source/dr_strange_algorithm.js
@@ -1,110 +1,150 @@
-const videoElement = document.getElementsByClassName('input_video')[0];
-const canvasElement = document.getElementsByClassName('output_canvas')[0];
-const canvasCtx = canvasElement.getContext('2d');
-var mainCanvas = document.getElementsByClassName('main_canvas')[0];
-var mainCanvasCtx = mainCanvas.getContext('2d');
-const indices = {"WRIST": 0,
-  "THUMB_CMC": 1,
-  "THUMB_MCP": 2,
-  "THUMB_IP": 3,
-  "THUMB_TIP": 4,
-  "INDEX_FINGER_MCP": 5,
-  "INDEX_FINGER_PIP": 6,
-  "INDEX_FINGER_DIP": 7,
-  "INDEX_FINGER_TIP": 8,
-  "MIDDLE_FINGER_MCP": 9,
-  "MIDDLE_FINGER_PIP": 10,
-  "MIDDLE_FINGER_DIP": 11,
-  "MIDDLE_FINGER_TIP": 12,
-  "RING_FINGER_MCP": 13,
-  "RING_FINGER_PIP": 14,
-  "RING_FINGER_DIP": 15,
-  "RING_FINGER_TIP": 16,
-  "PINKY_MCP": 17,
-  "PINKY_PIP": 18,
-  "PINKY_DIP": 19,
-  "PINKY_TIP": 20
-}
+const videoElement = document.getElementsByClassName("input_video")[0];
+
+const canvasElement = document.getElementsByClassName("output_canvas")[0];
+const canvasCtx = canvasElement.getContext("2d");
+
+const secondaryCanvas = document.getElementsByClassName("secondary_canvas")[0];
+const secondaryCanvasCtx = secondaryCanvas.getContext("2d");
+
+const mainCanvas = document.getElementsByClassName("main_canvas")[0];
+const mainCanvasCtx = mainCanvas.getContext("2d");
+
+const scoreElement = document.getElementById("score");
+const locationElement = document.getElementById("location");
+const data = {
+  1: "Kharkiv",
+  2: "The Petro Sahaidachny Monument in Kyiv",
+  3: "Kharkiv",
+  4: "Kyiv",
+  5: "Kharkiv",
+  6: "Lviv",
+  7: "Kharkiv",
+  8: "Bakhmut",
+  9: "Mariupol"
+};
+let data_point;
+const indices = {
+  WRIST: 0,
+  THUMB_CMC: 1,
+  THUMB_MCP: 2,
+  THUMB_IP: 3,
+  THUMB_TIP: 4,
+  INDEX_FINGER_MCP: 5,
+  INDEX_FINGER_PIP: 6,
+  INDEX_FINGER_DIP: 7,
+  INDEX_FINGER_TIP: 8,
+  MIDDLE_FINGER_MCP: 9,
+  MIDDLE_FINGER_PIP: 10,
+  MIDDLE_FINGER_DIP: 11,
+  MIDDLE_FINGER_TIP: 12,
+  RING_FINGER_MCP: 13,
+  RING_FINGER_PIP: 14,
+  RING_FINGER_DIP: 15,
+  RING_FINGER_TIP: 16,
+  PINKY_MCP: 17,
+  PINKY_PIP: 18,
+  PINKY_DIP: 19,
+  PINKY_TIP: 20,
+};
+
 let window_height = window.innerHeight;
 let window_width = window.innerWidth;
-mainCanvas.width  = window_width;
+
+mainCanvas.width = window_width;
 mainCanvas.height = window_height;
-const SCORE_THRESHOLD=0.92;
-var PATH = [];
+
+secondaryCanvas.width = window_width;
+secondaryCanvas.height = window_height;
+
+const SCORE_THRESHOLD = 0.92;
+let PATH = [];
 const PATH_LIFETIME = 1000;
-var over = false;
-const RADIUS = (window_height < window_width) ? (window_height/2 - 20) : (window_width/2 - 20);
+let over = false;
+const RADIUS =
+  window_height < window_width ? window_height / 2 - 20 : window_width / 2 - 20;
 var score = NaN;
 var correct = {};
 var background = new Image();
+var secondaryBackground = new Image();
 
-function setBackground(){
-  let number = Math.floor(Math.random()*8 + 1);
-  number = 4;
-  let string = '/DB/' + number + '/before.jpg';
-  console.log(string);
+function setBackground() {
+  let number = Math.floor(Math.random() * 7 + 1);
+  data_point = number;
+  let string = "/DB/" + number + "/before.jpg";
+  let secondaryString = "/DB/" + number + "/after.jpg";
   background.src = string;
+  secondaryBackground.src = secondaryString;
+  locationElement.innerText = `This was ${data[number]}`;
 }
 
 function drawImageScaled(img, ctx) {
-  var canvas = ctx.canvas ;
-  var hRatio = canvas.width  / img.width;
-  var vRatio =  canvas.height / img.height;
-  var ratio  = Math.min ( hRatio, vRatio);
-  var centerShift_x = ( canvas.width - img.width*ratio ) / 2;
-  var centerShift_y = ( canvas.height - img.height*ratio ) / 2;
-  ctx.clearRect(0,0,canvas.width, canvas.height);
-  ctx.drawImage(img, 0,0, img.width, img.height,
-    centerShift_x,centerShift_y,img.width*ratio, img.height*ratio);
+  var canvas = ctx.canvas;
+  var hRatio = canvas.width / img.width;
+  var vRatio = canvas.height / img.height;
+  var ratio = Math.min(hRatio, vRatio);
+  var centerShift_x = (canvas.width - img.width * ratio) / 2;
+  var centerShift_y = (canvas.height - img.height * ratio) / 2;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    img,
+    0,
+    0,
+    img.width,
+    img.height,
+    centerShift_x,
+    centerShift_y,
+    img.width * ratio,
+    img.height * ratio
+  );
 }
 
-function getPolygonLength(){
+function getPolygonLength() {
   let length = 0;
-  for (let i = 0; i < PATH.length; i++){
-    let next = i+1;
-    if (next === PATH.length){
+  for (let i = 0; i < PATH.length; i++) {
+    let next = i + 1;
+    if (next === PATH.length) {
       next = 0;
     }
-    length+=dist(PATH[i], PATH[next]);
+    length += dist(PATH[i], PATH[next]);
   }
   return length;
 }
 
-function getPolygonArea(){
+function getPolygonArea() {
   let area = 0;
-  for (let i = 0; i < PATH.length; i++){
-    let next = i+1;
-    if (next === PATH.length){
+  for (let i = 0; i < PATH.length; i++) {
+    let next = i + 1;
+    if (next === PATH.length) {
       next = 0;
     }
-    area+=PATH[i][0]*PATH[next][1];
-    area-=PATH[i][1]*PATH[next][0];
+    area += PATH[i][0] * PATH[next][1];
+    area -= PATH[i][1] * PATH[next][0];
   }
-  area = Math.abs(area)/2;
+  area = Math.abs(area) / 2;
   return area;
 }
 
-function calculateRoundness(){
+function calculateRoundness() {
   const area = getPolygonArea();
   const length = getPolygonLength();
-  const R = length/(2*Math.PI);
-  const circle_area = Math.PI*R*R;
+  const R = length / (2 * Math.PI);
+  const circle_area = Math.PI * R * R;
   return area / circle_area;
 }
 
-function keepRecentPartOfPath(){
+function keepRecentPartOfPath() {
   const now = new Date().getTime();
-  while(PATH.length > 0 && now-PATH[0][2] > PATH_LIFETIME){
+  while (PATH.length > 0 && now - PATH[0][2] > PATH_LIFETIME) {
     PATH.shift();
   }
 }
 
-function dist(a, b){
+function dist(a, b) {
   return Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2));
 }
 
 function correctPosition(results) {
-  let correct = {"Left": false, "Right": false};
+  let correct = { Left: false, Right: false };
 
   const handedness = results.multiHandedness;
   let mcp_check;
@@ -113,39 +153,75 @@ function correctPosition(results) {
   for (const [hand_index, hand_info] of handedness.entries()) {
     let hand_label = hand_info.label;
     let hand_landmarks = results.multiHandLandmarks[hand_index];
-    let dtip = distance(hand_landmarks[indices.INDEX_FINGER_TIP],
-      hand_landmarks[indices.MIDDLE_FINGER_TIP])
-    let ddip = distance(hand_landmarks[indices.INDEX_FINGER_DIP],
-      hand_landmarks[indices.MIDDLE_FINGER_DIP])
-    let dpip = distance(hand_landmarks[indices.INDEX_FINGER_PIP],
-      hand_landmarks[indices.MIDDLE_FINGER_PIP])
+    let dtip = distance_3d(
+      hand_landmarks[indices.INDEX_FINGER_TIP],
+      hand_landmarks[indices.MIDDLE_FINGER_TIP]
+    );
+    let ddip = distance_3d(
+      hand_landmarks[indices.INDEX_FINGER_DIP],
+      hand_landmarks[indices.MIDDLE_FINGER_DIP]
+    );
+    let dpip = distance_3d(
+      hand_landmarks[indices.INDEX_FINGER_PIP],
+      hand_landmarks[indices.MIDDLE_FINGER_PIP]
+    );
 
-    let index_check = (hand_landmarks[indices.INDEX_FINGER_TIP].y < hand_landmarks[indices.INDEX_FINGER_DIP].y) &&
-      (hand_landmarks[indices.INDEX_FINGER_DIP].y < hand_landmarks[indices.INDEX_FINGER_PIP].y) &&
-      (hand_landmarks[indices.INDEX_FINGER_PIP].y < hand_landmarks[indices.INDEX_FINGER_MCP].y)
+    let index_check =
+      hand_landmarks[indices.INDEX_FINGER_TIP].y <
+        hand_landmarks[indices.INDEX_FINGER_DIP].y &&
+      hand_landmarks[indices.INDEX_FINGER_DIP].y <
+        hand_landmarks[indices.INDEX_FINGER_PIP].y &&
+      hand_landmarks[indices.INDEX_FINGER_PIP].y <
+        hand_landmarks[indices.INDEX_FINGER_MCP].y;
 
-    let middle_check = (hand_landmarks[indices.MIDDLE_FINGER_TIP].y < hand_landmarks[indices.MIDDLE_FINGER_DIP].y) &&
-      (hand_landmarks[indices.MIDDLE_FINGER_DIP].y < hand_landmarks[indices.MIDDLE_FINGER_PIP].y) &&
-      (hand_landmarks[indices.MIDDLE_FINGER_PIP].y < hand_landmarks[indices.MIDDLE_FINGER_MCP].y)
+    let middle_check =
+      hand_landmarks[indices.MIDDLE_FINGER_TIP].y <
+        hand_landmarks[indices.MIDDLE_FINGER_DIP].y &&
+      hand_landmarks[indices.MIDDLE_FINGER_DIP].y <
+        hand_landmarks[indices.MIDDLE_FINGER_PIP].y &&
+      hand_landmarks[indices.MIDDLE_FINGER_PIP].y <
+        hand_landmarks[indices.MIDDLE_FINGER_MCP].y;
 
     if (hand_label === "Left") {
-      mcp_check = hand_landmarks[indices.INDEX_FINGER_MCP].x > hand_landmarks[indices.MIDDLE_FINGER_MCP].x;
-      if (mcp_check && index_check && middle_check && Math.abs(dtip - ddip) < 0.01 && Math.abs(dtip - dpip) < 0.009 && Math.abs(dpip - ddip) < 0.009) {
+      mcp_check =
+        hand_landmarks[indices.INDEX_FINGER_MCP].x >
+        hand_landmarks[indices.MIDDLE_FINGER_MCP].x;
+      if (
+        mcp_check &&
+        index_check &&
+        middle_check &&
+        Math.abs(dtip - ddip) < 0.01 &&
+        Math.abs(dtip - dpip) < 0.009 &&
+        Math.abs(dpip - ddip) < 0.009
+      ) {
         correct["Right"] = true;
-        rightPoint = [Math.abs(hand_landmarks[indices.INDEX_FINGER_TIP].x*window_width), Math.abs(hand_landmarks[indices.INDEX_FINGER_TIP].y*window_height), new Date().getTime()];
+        rightPoint = [
+          Math.abs(hand_landmarks[indices.INDEX_FINGER_TIP].x * window_width),
+          Math.abs(hand_landmarks[indices.INDEX_FINGER_TIP].y * window_height),
+          new Date().getTime(),
+        ];
       }
     }
     if (hand_label === "Right") {
-      mcp_check = hand_landmarks[indices.INDEX_FINGER_MCP].x < hand_landmarks[indices.MIDDLE_FINGER_MCP].x;
-      if (mcp_check && index_check && middle_check && Math.abs(dtip - ddip) < 0.03 && Math.abs(dtip - dpip) < 0.009 && Math.abs(dpip - ddip) < 0.009) {
+      mcp_check =
+        hand_landmarks[indices.INDEX_FINGER_MCP].x <
+        hand_landmarks[indices.MIDDLE_FINGER_MCP].x;
+      if (
+        mcp_check &&
+        index_check &&
+        middle_check &&
+        Math.abs(dtip - ddip) < 0.03 &&
+        Math.abs(dtip - dpip) < 0.009 &&
+        Math.abs(dpip - ddip) < 0.009
+      ) {
         correct["Left"] = true;
       }
     }
-    i+=1;
-    if (i === 2 && correct["Right"] && correct["Left"]){
-      if(PATH.length !== 0 && dist(PATH[PATH.length - 1], rightPoint) > 20) {
+    i += 1;
+    if (i === 2 && correct["Right"] && correct["Left"]) {
+      if (PATH.length !== 0 && dist(PATH[PATH.length - 1], rightPoint) > 20) {
         PATH.push(rightPoint);
-      }else if(PATH.length === 0){
+      } else if (PATH.length === 0) {
         PATH.push(rightPoint);
       }
     }
@@ -154,8 +230,10 @@ function correctPosition(results) {
   return correct;
 }
 
-function distance(a, b){
-  return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2) + Math.pow(a.z - b.z, 2))
+function distance_3d(a, b) {
+  return Math.sqrt(
+    Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2) + Math.pow(a.z - b.z, 2)
+  );
 }
 
 function drawHandEffects(position, radius) {
@@ -173,41 +251,113 @@ function drawHandEffects(position, radius) {
   mainCanvasCtx.stroke();
 }
 
-function drawCircle(){
+function drawCircle(i = 0) {
+  mainCanvasCtx.globalCompositeOperation = "destination-out";
   mainCanvasCtx.beginPath();
-  mainCanvasCtx.fillStyle = 'red';
-  mainCanvasCtx.arc(window_width / 2, window_height / 2, RADIUS * score, 0, 2 * Math.PI);
+  mainCanvasCtx.arc(
+    window_width / 2,
+    window_height / 2,
+    RADIUS * score + i,
+    0,
+    2 * Math.PI
+  );
   mainCanvasCtx.fill();
+  mainCanvasCtx.globalCompositeOperation = "source-over";
+}
+
+function expandCircle() {
+  const animationDuration = 3000; // 3 seconds
+  const fps = 60; // Frames per second
+  const totalFrames = (animationDuration / 1000) * fps;
+  const radiusIncrement = (RADIUS * 2) / totalFrames;
+
+  let currentFrame = 0;
+  let radius = 0;
+
+  function drawFrame() {
+    if (currentFrame < totalFrames) {
+      // Clear the main canvas
+      mainCanvasCtx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+
+      // Draw the erasing circle
+      mainCanvasCtx.globalCompositeOperation = "destination-out";
+      mainCanvasCtx.beginPath();
+      mainCanvasCtx.arc(
+        mainCanvas.width / 2,
+        mainCanvas.height / 2,
+        radius,
+        0,
+        2 * Math.PI
+      );
+      mainCanvasCtx.fill();
+
+      // Increase the radius for the next frame
+      radius += radiusIncrement;
+      currentFrame++;
+
+      // Request the next frame
+      requestAnimationFrame(drawFrame);
+    } else {
+      // Animation complete, hide the main canvas and show the secondary canvas
+      mainCanvas.style.display = "none";
+      secondaryCanvas.style.display = "block";
+    }
+  }
+
+  // Start the animation
+  requestAnimationFrame(drawFrame);
 }
 
 function onResults(results) {
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(
+    results.image,
+    0,
+    0,
+    canvasElement.width,
+    canvasElement.height
+  );
+  let string;
+  if (isNaN(score)) {
+    string = "0";
+  } else if (score > SCORE_THRESHOLD) {
+    string = "100";
+  } else {
+    string = Math.floor(score * 100);
+  }
+  scoreElement.innerText = `${string}% of the portal`;
 
   if (!over) {
     mainCanvasCtx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
     drawImageScaled(background, mainCanvasCtx);
+    drawImageScaled(secondaryBackground, secondaryCanvasCtx);
     if (!isNaN(score)) {
       drawCircle();
-      if (score > SCORE_THRESHOLD){
+      if (score > SCORE_THRESHOLD) {
         over = true;
         console.log("Over:" + over);
       }
     }
-    correct = {"Left": false, "Right": false};
+    correct = { Left: false, Right: false };
     score = calculateRoundness();
   }
   if (results.multiHandLandmarks) {
-    if(over){
-      mainCanvasCtx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
-      drawImageScaled(background, mainCanvasCtx);
-      drawCircle();
+    if (over) {
+      camera.stop();
+      canvasElement.style.opacity = "0";
+      locationElement.innerText = `This is ${data[data_point]}`;
+      // mainCanvasCtx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+      // drawImageScaled(background, mainCanvasCtx);
+      expandCircle();
     }
     correct = correctPosition(results);
     keepRecentPartOfPath();
     for (const landmarks of results.multiHandLandmarks) {
-      const real_values = [landmarks[indices.INDEX_FINGER_TIP].x * window_width, landmarks[indices.INDEX_FINGER_TIP].y * window_height];
+      const real_values = [
+        landmarks[indices.INDEX_FINGER_TIP].x * window_width,
+        landmarks[indices.INDEX_FINGER_TIP].y * window_height,
+      ];
       drawHandEffects(real_values, 50);
     }
   }
@@ -215,27 +365,29 @@ function onResults(results) {
   canvasCtx.restore();
 }
 
-const hands = new Hands({locateFile: (file) => {
+const hands = new Hands({
+  locateFile: (file) => {
     return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-  }});
+  },
+});
 
 hands.setOptions({
   maxNumHands: 2,
   modelComplexity: 1,
   minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5
+  minTrackingConfidence: 0.5,
 });
 
 hands.onResults(onResults);
 
 const camera = new Camera(videoElement, {
   onFrame: async () => {
-    await hands.send({image: videoElement});
+    await hands.send({ image: videoElement });
   },
   width: window_width,
-  height: window_height
+  height: window_height,
 });
 
 background.onload = () => {
   camera.start();
-}
+};
--- js_source/positioned_functions.js
+++ js_source/positioned_functions.js
@@ -1,13 +1,6 @@
-var header = document.getElementById("header");
 var donateBtn = document.getElementById("donate");
 var shareBtn = document.getElementById("share");
-var pressed = 0;
-var visible = true;
-
-document.addEventListener('mousemove', (event) => {
-  // console.log(event.clientX, event.clientY);
-  onMove(event.clientX, event.clientY);
-});
+var nextBtn = document.getElementById("next");
 
 donateBtn.addEventListener('click', () => {
   window.open("https://www.google.com");
@@ -17,49 +10,6 @@ shareBtn.addEventListener('click', () => {
   window.open("https://www.google.com");
 });
 
-function show(){
-  $(".appbar").css({opacity: 0, visibility: "visible"}).animate({opacity: 1.0}, 200);
-  header.toggleAttribute("m-fadeOut");
-  // donateBtn.animate({opacity: 0}, 200)
-  // header.animate({opacity: 0}, 200)
-}
-
-function hide(){
-  $(".appbar").css({opacity: 1, visibility: "visible"}).animate({opacity: 0.0}, 200);
-  // header.animate({opacity: 0}, 200);
-}
-
-function onMove(X, Y){
-  if (Y < header.clientHeight){
-    if(visible === false){
-      show();
-      visible = true;
-      console.log(visible);
-    }
-    if(donateBtn.offsetLeft < X  && X < donateBtn.offsetLeft + donateBtn.offsetWidth){
-      if(pressed === 200) {
-        pressed = 0;
-        donateBtn.click();
-      }else{
-        pressed+=1;
-      }
-    }
-    else if (shareBtn.offsetLeft < X && X < shareBtn.offsetLeft + shareBtn.offsetWidth){
-      if(pressed === 100) {
-        pressed = 0;
-        shareBtn.click();
-      }else{
-        pressed+=1;
-      }
-    }else{
-      pressed = 0;
-    }
-  }else{
-    if(visible === true){
-      console.log(visible);
-      hide();
-      visible = false;
-      console.log(visible);
-    }
-  }
-}
+nextBtn.addEventListener('click', () => {
+  location.reload();
+});
